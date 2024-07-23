87ecb80ac2f3c3f19f5b0c9d3e58c884ce4e036b
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2024-07-23 07:49:42 +0530

    WIP: bio section

---
+++ src/components/BioSection/index.jsx
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const BioSection = () => {
+  return <div id="bio-section">BioSection</div>;
+};
+
+export default BioSection;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,12 @@
 import AboutSection from "../../components/AboutSection";
+import BioSection from "../../components/BioSection";
+
 import "./style.css";
 const home = () => {
   return (
     <div className="home-page">
       <AboutSection />
+      <BioSection />
     </div>
   );
 };
