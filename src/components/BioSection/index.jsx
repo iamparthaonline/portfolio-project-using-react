3fa31f899560cce40c5fdbddfdebfcae6c8bd4ee
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2024-07-23 07:51:56 +0530

    Completed Bio Section

--- src/components/BioSection/index.jsx
+++ src/components/BioSection/index.jsx
@@ -1,7 +1,13 @@
 import "./style.css";
 
 const BioSection = () => {
-  return <div id="bio-section">BioSection</div>;
+  return (
+    <div id="bio-section">
+      Aloha, I am John Doe, software engineer by profession, working as an MLOps
+      Engineer at Amazon Private Limited, and very good at working with DevOps,
+      Container Technology AWS Cloud Computing, Infrastructure as code(Iac).
+    </div>
+  );
 };
 
 export default BioSection;
